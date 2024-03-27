284fa703b570e2bf5e685a82d753caa51e98d706
Emma Vriezen <49442496+EmmaVriezen@users.noreply.github.com>
2024-03-27 12:57:53 +0100

    Show selected file paths

--- src/components/FileSelection.vue
+++ src/components/FileSelection.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { ref, computed } from "vue";
 import { open } from '@tauri-apps/api/dialog';
 
-const fileSelection = ref("Select files...");
-const count = ref(0);
+const fileSelection = ref<String[]>([]);
 
-const selected = async () => {
+const count = computed(() => fileSelection.value.length);
+
+const buttonLabel = computed(() => {
+    if (count.value === 1) {
+        return count.value + " file selected";
+    } else if (count.value) {
+        return count.value + " files selected";
+    } else {
+        return "Select files...";
+    }
+});
+
+const selectImages = async () => {
     let selection = await open({
         multiple: true,
         filters: [{
@@ -14,19 +25,16 @@ const selected = async () => {
         }]
     });
     if (Array.isArray(selection)) {
-        fileSelection.value = selection[0];
-        count.value = selection.length;
-    } else if (selection === null) {
-        fileSelection.value = "Select files...";
-        count.value = 0;
-    } else {
         fileSelection.value = selection;
-        count.value = 1;
+    } else if (selection === null) {
+        fileSelection.value = [];
+    } else {
+        fileSelection.value = [selection];
     }
 }
 </script>
 
 <template>
-    <p>Number of selected images: {{ count }}</p>
-    <button type="button" @click="selected">{{ fileSelection }}</button>
+    <button type="button" @click="selectImages">{{ buttonLabel }}</button>
+    <p>{{ fileSelection }}</p>
 </template>
