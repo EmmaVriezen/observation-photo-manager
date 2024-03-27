ae690beeaf79dedc02b8694729db075586857cef
Emma Vriezen <49442496+EmmaVriezen@users.noreply.github.com>
2024-03-27 16:31:55 +0100

    Emit destination folder path to App Vue component

--- src/components/FolderSelection.vue
+++ src/components/FolderSelection.vue
@@ -1,29 +1,22 @@
 <script setup lang="ts">
-import { ref, computed } from 'vue'
 import { open } from '@tauri-apps/api/dialog'
 
-const selectedFolder = ref('')
-
-const folderSelectionButtonLabel = computed(() => {
-  if (selectedFolder.value === '') {
-    return 'Select folder...'
-  } else {
-    return selectedFolder.value
-  }
-})
+const emit = defineEmits(['setFolder'])
 
 const selectFolder = async (): Promise<void> => {
   const selection = await open({ directory: true, multiple: false })
   if (Array.isArray(selection)) {
     console.error('Selecting multiple folders is forbidden.')
   } else if (selection === null) {
-    selectedFolder.value = ''
+    emit('setFolder', '')
   } else {
-    selectedFolder.value = selection
+    emit('setFolder', selection)
   }
 }
 </script>
 
 <template>
-  <button type="button" @click="selectFolder">{{ folderSelectionButtonLabel }}</button>
+  <button type="button" @click="selectFolder">
+    <slot></slot>
+  </button>
 </template>
